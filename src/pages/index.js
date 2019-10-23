972863b0db88045a40ebe92f9d373d3efd334adb
Aluno <aluno@tub-depe-a0717.tubarao.ifsc.edu.br>
2019-10-23 19:32:01 -0300

    primeiro commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import SEO from "../components/Seo";
+
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+    </Layout>
+  );
+}
+
+export default IndexPage;
